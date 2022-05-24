06d1271b078b5f43cbdd07136e313ef162b1227f
Colin Joss <colindjoss@gmail.com>
2022-05-23 17:26:54 -0700

    working assignment

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ COPY requirements.txt ./
 RUN pip install -r requirements.txt
 COPY . .
 ENV PORT=8080
-ENV GOOGLE_APPLICATION_CREDENTIALS='./jossc-8-c1a075c74e50.json'
+ENV GOOGLE_APPLICATION_CREDENTIALS='./jossc-8-1b54f583e7ce.json'
 EXPOSE ${PORT}
 CMD [ "python", "main.py" ]
--- boat.py
+++ boat.py
@@ -1,10 +1,8 @@
 from flask import Blueprint, request, make_response
 from google.cloud import datastore
 import json
-from json2html import json2html
 from constants import *
-from common_functions import get_entity, is_nonexistent, name_in_use, valid, \
-    response_object, new_boat
+from common_functions import get_entity, is_nonexistent, response_object, new_boat
 
 client = datastore.Client()
 bp = Blueprint('boat', __name__, url_prefix='/boats')
@@ -22,15 +20,8 @@ def get_post_boats():
     elif request.method == 'POST':
         content = request.get_json()
         if 'name' in content and 'type' in content and 'length' in content:
-            if not valid('name', content['name']):
-                return {'Error': BAD_VALUE}, 400
-            if not valid('type', content['type']):
-                return {'Error': BAD_VALUE}, 400
-            if not valid('length', content['length']):
-                return {'Error': BAD_VALUE}, 400
             boat = new_boat(datastore, client, content)
             return response_object(make_response(json.dumps(boat)), AJSON, 201)
-
         else:
             return {'Error': MISSING_ATTR}, 400
 
@@ -59,42 +50,11 @@ def get_put_patch_delete(bid):
     elif request.method == 'POST':
         return {'Error': NOT_SUPPORTED + 'POST'}, 405
 
-    elif request.method == 'PATCH':
-        content = request.get_json()
-        if 'id' in content or 'self' in content:
-            return {'Error': NO_EDIT}, 403
-
-        if 'name' in content or 'type' in content or 'length' in content:
-            boat = get_entity(client, BOAT, bid)
-            if is_nonexistent(boat):
-                return NO_BOAT, 404
-            for prop in content:
-                if not valid(prop, content[prop]):
-                    return {'Error': BAD_VALUE}, 400
-                boat[prop] = content[prop]
-            res = response_object(make_response(json.dumps(boat)), AJSON, 200)
-            client.put(boat)
-            return res
-        else:
-            return {'Error': MISSING_ATTR}, 400
-
     elif request.method == 'PUT':
-        content = request.get_json()
-        if 'id' in content:
-            return {'Error': NO_EDIT}, 403
-        if 'name' in content and 'type' in content and 'length' in content:
-            boat = get_entity(client, BOAT, bid)
-            if boat is None:
-                return NO_BOAT, 404
-            for prop in content:
-                if not valid(prop, content[prop]):
-                    return {'Error': BAD_VALUE}, 400
-                boat[prop] = content[prop]
-            res = response_object(make_response(json.dumps(boat)), AJSON, 200)
-            client.put(boat)
-            return res
-        else:
-            return {'Error': MISSING_ATTR}, 400
+        return {'Error': NOT_SUPPORTED + 'PUT'}, 405
+
+    elif request.method == 'PATCH':
+        return {'Error': NOT_SUPPORTED + 'PATCH'}, 405
 
     elif request.method == 'DELETE':
         boat = get_entity(client, BOAT, bid)
